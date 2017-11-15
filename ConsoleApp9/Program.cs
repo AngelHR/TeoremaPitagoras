461489701ec44410086643854879e8912b298ad4
angel gabriel hernandez rodrigez <angel_n_n@live.com>
2017-11-14 23:07:47 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lado1, lado2;
+
+            Console.WriteLine("Ingresa el lado 1:");
+            lado1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el lado 2:");
+            lado2 = int.Parse(Console.ReadLine());
+
+            double r = Math.Sqrt((lado1 * lado1) + (lado2 * lado2));
+
+            Console.WriteLine($"la Hipotenusa es igual a {r}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
